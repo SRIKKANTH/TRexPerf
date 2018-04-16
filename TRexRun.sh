cb0d11598d24858ccd83e1bef7a558e6fdff6452
Srikanth Myakam <m.srikanth.m@gmail.com>
2018-04-15 20:00:18 -0700

    Fixed waiting time logic

--- TRexRun.sh
+++ TRexRun.sh
@@ -14,22 +14,31 @@ count=0
 
 while [ "x${yaml_files_list[$count]}" != "x" ]
 do
-LOGFILE=$LOGDIR/${yaml_files_list[$count]}.log
-echo "yaml_files_list: $count : ${yaml_files_list[$count]}: $LOGFILE"
+	LOGFILE=$LOGDIR/${yaml_files_list[$count]}.log
+	echo "yaml_files_list: $count : ${yaml_files_list[$count]}: $LOGFILE"
 
-./t-rex-64  -c 1  -m 10000000 -d ${duration} --nc --no-flow-control-change --active-flows 100000 -f cap2/${yaml_files_list[$count]} 2>&1 > $LOGFILE &
+	./t-rex-64  -c 1  -m 10000000 -d ${duration} --nc --no-flow-control-change --active-flows 100000 -f cap2/${yaml_files_list[$count]} 2>&1 > $LOGFILE &
 
-sleep 10
-if pgrep -x "t-rex-64" > /dev/null
-then
-	echo "Running"
-	sleep ${duration}
+	time_elapsed=0
+	while [[ x`pgrep -x "t-rex-64"` != "x" ]] && [[ $time_elapsed -lt ${duration} ]]
+	do
+		sleep 10
+		time_elapsed=$((time_elapsed + 10))
+		#echo $time_elapsed
+	done
 	pkill t-rex-64
-else
-	echo "Stopped"
-fi
 
-sleep 5
+#	sleep 10
+#	if pgrep -x "t-rex-64" > /dev/null
+#	then
+#		echo "Running"
+#		sleep ${duration}
+#		pkill t-rex-64
+#	else
+#		echo "Stopped"
+#	fi
 
-((count++))
+	sleep 5
+
+	((count++))
 done
